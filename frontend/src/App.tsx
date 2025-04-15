772e2b65f928e56fef0189b1816fd54fce722c0f
Haritha-sayone <haritha@sayonetech.com>
2025-04-15 17:46:51 +0530

    Protected pages from unauthorised users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Provider, useSelector } from 'react-redux';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -8,11 +8,18 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import ArticleList from './pages/ArticleList';
 import ArticleDetails from './pages/ArticleDetails';
-import SavedArticles from './pages/SavedArticles'; // Import SavedArticles page
+import SavedArticles from './pages/SavedArticles';
 import { ToastContainer } from 'react-toastify';
 import store from './store';
+import { RootState } from './store';
 import 'react-toastify/dist/ReactToastify.css';
 import './assets/styles/global.css';
+import { JSX } from 'react';
+
+const PrivateRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
 const App = () => {
   return (
@@ -20,13 +27,34 @@ const App = () => {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/saved-articles"
+            element={
+              <PrivateRoute>
+                <SavedArticles />
+              </PrivateRoute>
+            }
+          />
           <Route path="/articles" element={<ArticleList />} />
           <Route path="/articles/:id" element={<ArticleDetails />} />
-          <Route path="/saved-articles" element={<SavedArticles />} /> {/* Add route for SavedArticles */}
         </Routes>
         <Footer />
         <ToastContainer position="top-right" autoClose={3000} />
